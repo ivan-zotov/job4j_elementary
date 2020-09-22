401cc876fd8efb4f248b21c525180ac280d69b0b
EvilEvan <2ivanzotov@gmail.com>
2020-09-22 21:39:34 +0300

    1. Модульные тесты. [#363823]

--- src/ru/job4j/convert/Converter.java
+++ src/ru/job4j/convert/Converter.java
@@ -12,7 +12,7 @@ public class Converter {
 
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rebles is " + euro + " euro");
+        System.out.println("140 rubles is " + euro + " euro");
         int in = 140;
         int expected = 2;
         int out = Converter.rubleToEuro(in);
