d3f27fed05b5372f623cafa16eeb5bf9e6a09fad
EvilEvan <2ivanzotov@gmail.com>
2020-10-08 21:24:54 +0300

    6.2.2. Переставить элементы массива [#363777]

--- src/ru/job4j/SortSelected.java
+++ src/ru/job4j/SortSelected.java
@@ -2,15 +2,14 @@ package ru.job4j;
 
 import ru.job4j.array.FindLoop;
 import ru.job4j.array.MinDiapason;
+import ru.job4j.array.SwitchArray;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
-            int index = FindLoop.indexOf(data, min, 0, data.length - 1);
-            int temp = data[i];
-            data[i] = data[index];
-            data[index] = temp;
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
+            data = SwitchArray.swap(data, i, index);
         }
         return data;
     }
