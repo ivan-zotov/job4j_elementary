90990778292787cb267013fccb8b270e62131afb
EvilEvan <2ivanzotov@gmail.com>
2020-10-25 17:53:51 +0300

    3. Перегрузить метод max для трех чисел [#363949] - new class Max

---
+++ test/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax7to4Then7() {
+        int result = Max.max(7, 2);
+        assertEquals(result, 7);
+    }
+
+    @Test
+    public void whenMax4to2Then2() {
+        int result = Max.max(2, 4);
+        assertEquals(result, 4);
+    }
+
+    @Test
+    public void whenEqualsThen4() {
+        int result = Max.max(4, 4);
+        assertEquals(result, 4);
+    }
+}
