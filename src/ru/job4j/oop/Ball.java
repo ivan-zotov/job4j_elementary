548f3a72b4fed8f1020d7623883ad63552121ba4
EvilEvan <2ivanzotov@gmail.com>
2020-10-25 16:02:48 +0300

    1.6. Взаимодействие объектов [#363954]

---
+++ src/ru/job4j/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void goAwayFromHare(Hare hare) {
+    }
+
+    public void goAwayFromWolf(Wolf wolf) {
+    }
+
+    public void beEatenByFox(Fox fox) {
+    }
+}
